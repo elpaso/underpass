dbfc60b1be7512daaa20ac163912c2fbb1fde759
elpaso <elpaso@users.noreply.github.com>
2021-10-14 07:52:06 +0000

    deploy: 47660683961180eaf1d747c902cdca661c216c7e

--- search/functions_10.js
+++ search/functions_10.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['underpass_0',['Underpass',['../classunderpass_1_1Underpass.html#a6ff8973151530c2617d34330ebffc12d',1,'underpass::Underpass']]],
-  ['updatecreator_1',['updateCreator',['../classunderpass_1_1Underpass.html#a0d375ae6d7da7d1f79eade3c7c7debeb',1,'underpass::Underpass']]],
-  ['updatedatabase_2',['updateDatabase',['../classosm2pgsql_1_1Osm2Pgsql.html#a5185664362216473c619135363d53e90',1,'osm2pgsql::Osm2Pgsql']]]
+  ['way_0',['way',['../classimport_1_1OSMHandler.html#a9edd204ec3ee0c2f3e832f5ceb8d04d6',1,'import::OSMHandler']]],
+  ['writestate_1',['writeState',['../classunderpass_1_1Underpass.html#ab8d3926eda69fe9e37ae5ad1edd24ce4',1,'underpass::Underpass']]]
 ];
